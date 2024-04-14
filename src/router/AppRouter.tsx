85ed6dc1178860f78a957999da72d717084504b5
SebaschaM <schaquila@autonoma.edu.pe>
2024-04-14 15:14:36 -0500

    Update wildcard route in AppRouter to use Navigate component

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 
 export const AppRouter = () => {
@@ -6,7 +6,7 @@ export const AppRouter = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="*" element={<Home />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </BrowserRouter >
     );
